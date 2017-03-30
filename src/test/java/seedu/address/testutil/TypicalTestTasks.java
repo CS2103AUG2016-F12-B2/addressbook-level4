779c41f9834fcc0b7678786805f8d5c6e2e0cbc4
JingRong ( <jing_rong92@hotmail.com>
2017-03-30 15:44:40 +0800

    used exact timestamp for typicaltest adding

--- src/test/java/seedu/address/testutil/TypicalTestTasks.java
+++ src/test/java/seedu/address/testutil/TypicalTestTasks.java
@@ -41,10 +41,10 @@ public class TypicalTestTasks {
 
             // Manually added
             hoon = new TaskBuilder().withName("Hoon Meier")
-                    .withStart("today").withDeadline("tomorrow")
+                    .withStart("Thu Mar 30 12:43:24 2017").withDeadline("Sat Apr 1 12:43:24 2017")
                     .withPriority(1).withNotes("").withCompletion("false").build();
             ida = new TaskBuilder().withName("Ida Mueller")
-                    .withStart("today").withDeadline("tomorrow")
+                    .withStart("Thu Mar 30 12:43:24 2017").withDeadline("Sat Apr 1 12:43:24 2017")
                     .withPriority(1).withNotes("").withCompletion("false").build();
         } catch (IllegalValueException e) {
             e.printStackTrace();
