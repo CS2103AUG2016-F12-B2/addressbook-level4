05c7356ac02d320870468d8cb76d7c0e4161a261
JingRong ( <jing_rong92@hotmail.com>
2017-04-05 17:12:53 +0800

    Added EDIT test

--- src/test/java/guitests/UndoCommandTest.java
+++ src/test/java/guitests/UndoCommandTest.java
@@ -7,7 +7,10 @@ import static seedu.todoapp.logic.commands.DeleteCommand.MESSAGE_DELETE_TASK_SUC
 import org.junit.Test;
 
 import guitests.guihandles.TaskCardHandle;
+import seedu.todoapp.commons.exceptions.IllegalValueException;
+import seedu.todoapp.logic.commands.EditCommand;
 import seedu.todoapp.logic.commands.UndoCommand;
+import seedu.todoapp.testutil.TaskBuilder;
 import seedu.todoapp.testutil.TestTask;
 import seedu.todoapp.testutil.TestUtil;
 
@@ -20,6 +23,19 @@ public class UndoCommandTest extends ToDoAppGuiTest {
         TestTask taskToAdd = td.hoon;
         assertAddSuccess(taskToAdd, currentList);
         currentList = TestUtil.addTasksToList(currentList, taskToAdd);
+
+        assertUndoCommandSuccess(td.getTypicalTasks());
+    }
+
+    @Test
+    public void undo_edit_success() throws Exception {
+        String detailsToEdit = "Bobby t/husband";
+        int toDoAppIndex = 1;
+
+        TestTask editedTask = new TaskBuilder().withName("Bobby").withStart("Thu Mar 30 12:43:24 2017")
+                .withDeadline("Thu Mar 31 12:43:24 2017").withPriority(1).withTags("husband").withNotes("").build();
+
+        assertEditSuccess(toDoAppIndex, toDoAppIndex, detailsToEdit, editedTask);
 
         assertUndoCommandSuccess(td.getTypicalTasks());
     }
@@ -38,8 +54,6 @@ public class UndoCommandTest extends ToDoAppGuiTest {
     @Test
     public void undo_nothing_failure() {
         // Test UNDO-ing nothing
-        TestTask[] currentList = td.getTypicalTasks();
-
         commandBox.runCommand("undo");
         assertResultMessage(UndoCommand.MESSAGE_FAIL);
     }
@@ -62,7 +76,6 @@ public class UndoCommandTest extends ToDoAppGuiTest {
     public void undo_clear_success() {    
         // Test CLEAR
         //verify a non-empty list can be cleared
-        TestTask[] currentList = td.getTypicalTasks();
         assertClearCommandSuccess();
 
         assertUndoCommandSuccess(td.getTypicalTasks());
@@ -105,6 +118,30 @@ public class UndoCommandTest extends ToDoAppGuiTest {
         commandBox.runCommand("clear");
         assertListSize(0);
         assertResultMessage("ToDoApp has been cleared!");
+    }
+
+    /**
+     * Checks whether the edited person has the correct updated details.
+     *
+     * @param filteredTaskListIndex index of person to edit in filtered list
+     * @param toDoAppIndex index of person to edit in the address book.
+     *      Must refer to the same person as {@code filteredTaskListIndex}
+     * @param detailsToEdit details to edit the person with as input to the edit command
+     * @param editedTask the expected person after editing the person's details
+     */
+    private void assertEditSuccess(int filteredTaskListIndex, int toDoAppIndex,
+                                    String detailsToEdit, TestTask editedTask) {
+        commandBox.runCommand("edit " + filteredTaskListIndex + " " + detailsToEdit);
+
+        // confirm the new card contains the right data
+        TaskCardHandle editedCard = taskListPanel.navigateToTask(editedTask.getName().fullName);
+        assertMatching(editedTask, editedCard);
+
+        // confirm the list now contains all previous persons plus the person with updated details
+        TestTask[] expectedTasksList = td.getTypicalTasks();
+        expectedTasksList[toDoAppIndex - 1] = editedTask;
+        assertTrue(taskListPanel.isListMatching(expectedTasksList));
+        assertResultMessage(String.format(EditCommand.MESSAGE_EDIT_TASK_SUCCESS, editedTask));
     }
 
     /*
