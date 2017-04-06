796db9fda7134dba6022fed136510c68c51936e7
JingRong ( <jing_rong92@hotmail.com>
2017-04-06 21:35:51 +0800

    Implemented auto update

--- src/main/java/seedu/todoapp/model/RecurrentTaskManager.java
+++ src/main/java/seedu/todoapp/model/RecurrentTaskManager.java
@@ -1,14 +1,27 @@
 //@@author A0114395E
 package seedu.todoapp.model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import javafx.collections.ObservableList;
+import seedu.todoapp.commons.exceptions.IllegalValueException;
+import seedu.todoapp.model.person.Completion;
+import seedu.todoapp.model.person.Deadline;
+import seedu.todoapp.model.person.Name;
+import seedu.todoapp.model.person.Notes;
+import seedu.todoapp.model.person.Priority;
 import seedu.todoapp.model.person.ReadOnlyTask;
+import seedu.todoapp.model.person.Start;
+import seedu.todoapp.model.person.Task;
+import seedu.todoapp.model.person.Venue;
 import seedu.todoapp.model.tag.UniqueTagList;
 
 /**
@@ -72,7 +85,12 @@ public class RecurrentTaskManager {
         this.timer.schedule(new TimerTask() {
             public void run() {
                 // TODO: Update tasks
-                updateAllRecurringTimestamps();
+                try {
+                    updateAllRecurringTimestamps();
+                } catch (Exception e) {
+                    // Exception caught
+                    e.printStackTrace();
+                }
             }
         }, 0, this.interval);
     }
@@ -80,41 +98,106 @@ public class RecurrentTaskManager {
     /*
      * Helper function to update all recurring tasks' timestamps for start/deadline
      */
-    private void updateAllRecurringTimestamps() {
+    private void updateAllRecurringTimestamps() throws Exception {
         // Get all tasks
         ObservableList<ReadOnlyTask> taskList  = this.model.getToDoApp().getTaskList();
         for (int i = 0; i < taskList.size(); i++) {
             ReadOnlyTask task = taskList.get(i);
             // Check if current task has a recurring tag, and update the timestamp if required
-            updateTask(task);
+            updateTask(i, task);
         }
     }
 
     /*
      * Helper function to check if task is recurring, and update accordingly
      */
-    private void updateTask(ReadOnlyTask task) {
+    private void updateTask(int idx, ReadOnlyTask task) throws Exception {
         UniqueTagList tagList = task.getTags();
         for (int j = 0 ; j < tagList.asObservableList().size(); j++) {
             if (supportedTypes.contains(tagList.asObservableList().get(j).tagName)) {
                 // Recurrent tag detected
-                switch (tagList.asObservableList().get(j).tagName) {
-                    case DAILY_INTERVAL:
-                        System.out.println('d');
-                        break;
-                    case WEEKLY_INTERVAL:
-                        System.out.println('w');
-                        break;
-                    case MONTHLY_INTERVAL:
-                        System.out.println('m');
-                        break;
-                    case YEARLY_INTERVAL:
-                        System.out.println('y');
-                        break;
-                    default:
-                        break;
-                }    
+                Task newTask = this.updateTask(task, tagList.asObservableList().get(j).tagName);
+                if (newTask != null) {
+                    model.updateTask(idx, newTask);
+                }
             }
         }
+    }
+
+    /*
+     * Helper function to create new task with updated time
+     */
+    private Task updateTask(ReadOnlyTask oldTask, String recurrentType) throws ParseException, IllegalValueException {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("EEE MMM dd kk:mm:ss yyyy");
+        Date currentTime = new Date();
+        if (oldTask.getStart().hasDate() && oldTask.getDeadline().hasDate()) {
+            // If got both, update both
+            Date oldStart = oldTask.getStart().getDate();
+            Date oldDeadline = oldTask.getDeadline().getDate();
+
+            // Update only if past deadline
+            if (currentTime.after(oldDeadline)) {
+                Date newStartDate = getNextRecurrentDate(c, oldStart, recurrentType);
+                Date newDeadlineDate = getNextRecurrentDate(c, oldDeadline, recurrentType);
+                // Make new start/end classes
+                Start newStart = new Start(df.format(newStartDate));
+                Deadline newDeadline = new Deadline(df.format(newDeadlineDate));
+                return new Task(oldTask.getName(), newStart, newDeadline, 
+                        oldTask.getPriority(), oldTask.getTags(), oldTask.getNotes(),
+                        oldTask.getCompletion(), oldTask.getVenue());
+            }
+        } else if (oldTask.getStart().hasDate()) {
+            // Only start
+            Date oldStart = oldTask.getStart().getDate();
+            // Update only if past start date
+            if (currentTime.after(oldStart)) {
+                Date newStartDate = getNextRecurrentDate(c, oldStart, recurrentType);
+                // Make new start/end classes
+                Start newStart = new Start(df.format(newStartDate));
+                return new Task(oldTask.getName(), newStart, oldTask.getDeadline(), 
+                        oldTask.getPriority(), oldTask.getTags(), oldTask.getNotes(),
+                        oldTask.getCompletion(), oldTask.getVenue());
+            }
+        } else if (oldTask.getDeadline().hasDate()) {
+            // Only deadline
+            Date oldDeadline = oldTask.getDeadline().getDate();
+            // Update only if past start date
+            if (currentTime.after(oldDeadline)) {
+                Date newDeadlineDate = getNextRecurrentDate(c, oldDeadline, recurrentType);
+                // Make new start/end classes
+                Deadline newDeadline = new Deadline(df.format(newDeadlineDate));
+                return new Task(oldTask.getName(), oldTask.getStart(), newDeadline,
+                        oldTask.getPriority(), oldTask.getTags(), oldTask.getNotes(),
+                        oldTask.getCompletion(), oldTask.getVenue());
+            }
+        }
+        // No dates: No difference - return old task
+        System.out.println("no dates");
+        return null;
+    }
+
+    /*
+     * Helper function to move date forward by recurrent amount
+     */
+    private Date getNextRecurrentDate(Calendar c, Date oldDate, String recurrentType) {
+        c.setTime(oldDate);
+        switch (recurrentType) {
+            case DAILY_INTERVAL:
+                c.add(Calendar.DATE, 1);
+                break;
+            case WEEKLY_INTERVAL:
+                c.add(Calendar.DATE, 7);
+                break;
+            case MONTHLY_INTERVAL:
+                c.add(Calendar.MONTH, 1);
+                break;
+            case YEARLY_INTERVAL:
+                c.add(Calendar.YEAR, 1);
+                break;
+            default:
+                break;
+        }
+        return c.getTime();
     }
 }
