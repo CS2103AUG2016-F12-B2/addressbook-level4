e5032af7db7436cf5aafab94806d6006fc82b77c
JingRong ( <jing_rong92@hotmail.com>
2017-04-06 15:28:20 +0800

    Open venue in google maps instead

--- src/main/java/seedu/todoapp/ui/BrowserPanel.java
+++ src/main/java/seedu/todoapp/ui/BrowserPanel.java
@@ -1,5 +1,6 @@
 package seedu.todoapp.ui;
 
+import javafx.concurrent.Worker;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
@@ -30,12 +31,52 @@ public class BrowserPanel extends UiPart<Region> {
     }
 
     public void loadTaskPage(ReadOnlyTask task) {
-        loadPage("https://www.google.com.sg/#safe=off&q=" + task.getName().fullName.replaceAll(" ", "+"));
+        loadPage("https://www.google.com/maps/place/" + task.getVenue().value.replaceAll(" ", "%20"));
     }
 
     public void loadPage(String url) {
         browser.getEngine().load(url);
+        this.fontFix();
     }
+
+    //@@author: A0114395E
+    /*
+     * Helper function to hotfix the fonts (Open Sans) which are failing on MacOS.
+     * See Issue : https://github.com/se-edu/addressbook-level4/issues/374
+     * Solution: Wait for browser to load before executing JS
+     * This still doesn't work.. unfortunately
+     */
+    private void fontFix() {
+        // on ready function
+        browser.getEngine().getLoadWorker().stateProperty().addListener((obs, oldState, newState) -> {
+            if (newState == Worker.State.SUCCEEDED) {
+                // new page has loaded, process:
+                // Replace fonts when document is ready
+                final String javascriptCSSCode = "var style = document.createElement('style');";
+                final String javascriptTagCode = "var css = 'body { font-family: \\'arial,sans-serif\\'; }';";
+                final String javascriptHeadCode = "head = document.head || document.getElementsByTagName('head')[0];";
+                final String javascriptSetTagType = "style.type = 'text/css';";
+                final String javascriptAddCSSToStyleTag = "(style.styleSheet) ? style.styleSheet.cssText = css : style.appendChild(document.createTextNode(css));";
+                final String javascriptAppendStyleToHead = "head.appendChild(style);";
+                
+                // Create javascript function
+                final String javascriptFunction = "var changeStyle = function() {"
+                        .concat(javascriptCSSCode)
+                        .concat(javascriptTagCode)
+                        .concat(javascriptHeadCode)
+                        .concat(javascriptSetTagType)
+                        .concat(javascriptAddCSSToStyleTag)
+                        .concat(javascriptAppendStyleToHead)
+                        .concat(" }");
+                
+                // Run javascript function
+                browser.getEngine().executeScript(javascriptFunction);
+                // System.out.println(javascriptFunction);
+                browser.getEngine().executeScript("setTimeout(changeStyle, 2000);");
+            }
+        });
+    }
+    //@@author
 
     /**
      * Frees resources allocated to the browser.
